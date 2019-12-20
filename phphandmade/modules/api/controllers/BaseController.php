916eb9e17bdd847268893e6654d3f25195ba58b5
Alexander Koryakin <sanek31rus@gmail.com>
2019-12-20 22:52:37 +0300

    Добавил базовые контроллеры

---
+++ phphandmade/modules/api/controllers/BaseController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace app\modules\api\controllers;
+
+use yii\filters\ContentNegotiator;
+use yii\filters\Cors;
+use yii\filters\RateLimiter;
+use yii\filters\VerbFilter;
+use yii\web\Response;
+
+class BaseController extends \yii\rest\Controller
+{
+    public function behaviors()
+    {
+        return [
+            'contentNegotiator' => [
+                'class' => ContentNegotiator::className(),
+                'formats' => [
+                    'application/json' => Response::FORMAT_JSON,
+                    'application/xml' => Response::FORMAT_JSON,
+                ],
+            ],
+            'verbFilter' => [
+                'class' => VerbFilter::className(),
+                'actions' => $this->verbs(),
+            ],
+            'rateLimiter' => [
+                'class' => RateLimiter::className(),
+            ],
+            'cors' => [
+                'class' => Cors::class
+            ]
+        ];
+    }
+}
