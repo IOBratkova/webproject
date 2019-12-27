7f5a83a7487da2892f60dbcac253e06531095f43
Irina Bratkova <bratckova.i@yandex.ru>
2019-12-27 19:02:08 +0300

    Добавлена домашняя страница

--- phphandmade/frontend/src/views/Home.vue
+++ phphandmade/frontend/src/views/Home.vue
@@ -1,18 +1,87 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="main">
+    <div id="carouselExampleFade" class="carousel slide carousel-fade" data-ride="carousel">
+      <div class="carousel-inner">
+        <div class="carousel-item active">
+          <img src="img/index-carousel-flowers-bottle.jpg" class="d-block w-100" alt="...">
+          <div class="carousel-caption d-none d-md-block text-center text-radiance-lime-light">
+            <h1 style="font-family:Lobster;">MADEHANDMADE - твой помощник в рукоделии!</h1>
+            <h4>Здесь ты сможешь узнать, что такое фенечки и научиться их делать!</h4>
+            <h4>Если для тебя это не в новинку, то среди схем ты можешь найти</h4>
+            <h4>именно ту, которая нужна тебе!</h4>
+            <p></p>
+          </div>
+        </div>
+        <div class="carousel-item">
+          <img src="img/index-carousel-dream-catcher.jpg" class="d-block w-100" alt="...">
+          <div class="carousel-caption d-none d-md-block text-center text-radiance-yellow-light">
+            <h1 style="font-family:Lobster;">MADEHANDMADE - твой помощник в рукоделии!</h1>
+            <h4>Увидел где-то ловец снов и очень захотел себе такой?</h4>
+            <h4>Не знаешь, с чего начать, но очень хочешь научиться?</h4>
+            <h4>Раздел "Ловцы снов" поможет тебе, будь ты начинающим или опытным ловцом сновидений!</h4>
+            <p></p>
+          </div>
+        </div>
+        <div class="carousel-item">
+          <img src="img/index-carousel-friendship-brasletes-2.jpg" class="d-block w-100" alt="...">
+          <div class="carousel-caption d-none d-md-block text-center text-radiance-red-light">
+            <h1 style="font-family:Lobster;">MADEHANDMADE - твой помощник в рукоделии!</h1>
+            <h4>Красивые узоры на фенечках уже давно не сказка ;) </h4>
+            <h4>"Гейзер", "Шотландка" и большое количество других схем для косого плетения.</h4>
+            <h4>Выбирай схему, подбирай новые цвета и создавай!</h4>
+            <p></p>
+          </div>
+        </div>
+      </div>
+      <a class="carousel-control-prev" href="#carouselExampleFade" role="button" data-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="sr-only">Назад</span>
+      </a>
+      <a class="carousel-control-next" href="#carouselExampleFade" role="button" data-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="sr-only">Вперед</span>
+      </a>
+    </div>
+    <div class="container w-75 shadow rounded p-3 border border-coral">
+      <h1 style="font-family:Lobster;" class="text-center text-radiance-red">Лучшие уроки</h1>
+      <div class="row">
+        <div class="col-lg-3">
+          <div class="card border border-blue">
+            <img src="img/dc1.jpg" class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title font-weight-bold">Ловец снов</h5>
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-3">
+          <div class="card border border-green">
+            <img src="img/dc1.jpg" class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title font-weight-bold">Ловец снов</h5>
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-3">
+          <div class="card border border-yellow">
+            <img src="img/dc1.jpg" class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title font-weight-bold">Ловец снов</h5>
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-3">
+          <div class="card border border-pink">
+            <img src="img/dc1.jpg" class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title font-weight-bold">Ловец снов</h5>
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
-
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
-export default {
-  name: 'home',
-  components: {
-    HelloWorld
-  }
-}
-</script>
