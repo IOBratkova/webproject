4685439f138077431148e13d74e677637108afec
Irina Bratkova <bratckova.i@yandex.ru>
2019-12-28 14:55:04 +0300

    Добавлен переход на страницу ловцов снов

--- phphandmade/frontend/src/App.vue
+++ phphandmade/frontend/src/App.vue
@@ -15,7 +15,7 @@
           <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav mr-auto">
               <li class="nav-item">
-                <a class="nav-link" href="#">Ловцы снов</a>
+                <router-link class="nav-link" to="/dreamcatchers">Ловцы снов</router-link>
               </li>
               <li class="nav-item dropdown">
                 <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownFirst" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
