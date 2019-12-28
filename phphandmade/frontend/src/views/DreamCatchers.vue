9e94bd766b3b1567288796062aaa855fd6e18533
Irina Bratkova <bratckova.i@yandex.ru>
2019-12-28 15:18:18 +0300

    Создана страница ловцов снов

--- phphandmade/frontend/src/views/DreamCatchers.vue
+++ phphandmade/frontend/src/views/DreamCatchers.vue
@@ -1,5 +1,45 @@
 <template>
-<div>lol</div>
+  <div>
+    <img class="img-fluid img-logo my-n3" src="img/ls4.png">
+    <div class="container shadow rounded w-75 my-3 border border-yellow">
+      <div align="center" style="margin-bottom: 1rem">
+        <span class="text-radiance-yellow" style="font-family:Lobster, cursive; font-size: 50px" >Ловцы снов</span>
+      </div>
+
+      <div class="text-center">
+        <p>
+          Ловец снов (или "силки для снов") — индейский амулет, который защищает спящего от злых духов, а также болезней.
+          По преданиям, плохие сны запутываются в паутине, а хорошие проскальзывают сквозь отверстие в середине.
+          На протяжении долгого времени многие племена претендовали на авторство ловцов снов.
+          Наиболее известными среди них были навахо, лакота сиу, гуроны, кри, мохоки, чироки, ирокезы и оджибве.
+          В начале двухтысячных годов было достигнуто согласие в том,
+          что изначальное авторство ловцов снов принадлежит племени оджибве.
+        </p>
+      </div>
+
+      <div class="row row-content-left mb-5 jumbotron-position shadow rounded-bookmark border border-lime">
+        <div align="center" style="margin-bottom: 1rem; margin-right: 1rem">
+          <span style="font-family:Lobster, cursive; font-size:50px;" >Фотоуроки</span>
+        </div>
+        <picture>
+          <img src="img/lovec-snov.png" height="100" weight="100">
+          <img src="img/ls5.png" height="100" weight="100">
+          <img src="img/ls2.png" height="100" weight="100">
+        </picture>
+      </div>
+
+      <div class="row row-content-right mb-5 jumbotron-position shadow rounded-bookmark border border-blue">
+        <picture>
+          <img src="static/images/ls.png" height="100" weight="100">
+          <img src="static/images/ls3.png" height="100" weight="100">
+          <img src="static/images/ls4.png" height="100" weight="100">
+        </picture>
+        <div align="center" style="margin-bottom: 1rem; margin-left: 1rem">
+          <span style="font-family:Lobster, cursive; Font-size:50px;" >Видеоуроки</span>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
