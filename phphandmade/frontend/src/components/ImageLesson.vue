dd7d5057d26f91653665279b7d9e06915d90810d
Irina Bratkova <bratckova.i@yandex.ru>
2019-12-29 00:03:55 +0300

    ImageLesson в PhotoLesson

---
+++ phphandmade/frontend/src/components/ImageLesson.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <div class="mt-2">
+      <p>Это текст!!!!</p>
+    </div>
+    <div class="mb-3">
+      <img src="img/dc1.jpg">
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'ImageLesson'
+}
+</script>
+
+<style scoped>
+
+</style>
--- phphandmade/frontend/src/views/PhotoLesson.vue
+++ phphandmade/frontend/src/views/PhotoLesson.vue
@@ -3,40 +3,34 @@
     <img class="img-fluid img-logo my-n3" src="img/ls4.png">
     <div class="container shadow rounded w-75 my-3 border border-yellow">
       <div class="mb-2" align="center" style="margin-bottom: 1rem; height: 40px">
-        <span class="text-radiance-lime" style="font-family:Lobster, cursive; Font-size:50px;" > {{model.title}}</span>
+        <span class="text-radiance-lime" style="font-family:Lobster, cursive; font-size:50px;" > {{model.title}}</span>
       </div>
 
       <div class="mt-4" align="center">
-        <span style="margin-top: 1rem; font-family:Lobster, cursive; Font-size:22px;" > Автор: {{user.login}}</span>
+        <span style="margin-top: 1rem; font-family:Lobster, cursive; font-size:22px;" > Автор: {{user.login}}</span>
       </div>
 
       <div class="m-1" align="center">
-        <span style="font-family:Lobster, cursive; Font-size:22px;" > Категория: {{type.title}}</span>
+        <span style="font-family:Lobster, cursive; font-size:22px;" > Категория: {{type.title}}</span>
       </div>
 
       <div class="row row-content-center mb-3 p-1 jumbotron-position shadow rounded-bookmark border border-pink">
         <div align="center" style="margin-bottom: 1rem; margin-left: 1rem">
-          <span class="text-radiance-blue" style="font-family:Lobster, cursive; Font-size:27px;" >Краткое описание урока</span>
+          <span class="text-radiance-blue" style="font-family:Lobster, cursive; font-size:27px;" >Краткое описание урока</span>
           <p>{{model.description}}</p>
         </div>
       </div>
 
       <div class="row row-content-center mb-3 p-1 jumbotron-position shadow rounded-bookmark border border-green">
         <div align="center" style="margin-bottom: 1rem; margin-left: 1rem">
-          <span class="text-radiance-yellow" style="font-family:Lobster, cursive; Font-size:27px;" >Материалы</span>
+          <span class="text-radiance-yellow" style="font-family:Lobster, cursive; font-size:27px;" >Материалы</span>
           <p>{{model.materials}}</p>
         </div>
       </div>
 
-      <div class="text-center">
-        <p>
-          Ловец снов (или "силки для снов") — индейский амулет, который защищает спящего от злых духов, а также болезней.
-          По преданиям, плохие сны запутываются в паутине, а хорошие проскальзывают сквозь отверстие в середине.
-          На протяжении долгого времени многие племена претендовали на авторство ловцов снов.
-          Наиболее известными среди них были навахо, лакота сиу, гуроны, кри, мохоки, чироки, ирокезы и оджибве.
-          В начале двухтысячных годов было достигнуто согласие в том,
-          что изначальное авторство ловцов снов принадлежит племени оджибве.
-        </p>
+      <div class="mt-4" align="center">
+        <span class="text-radiance-pink" style="margin-top: 1rem; font-family:Lobster, cursive; font-size:22px;" >Ход работы</span>
+        <ImageLesson/>
       </div>
     </div>
   </div>
@@ -44,8 +38,10 @@
 
 <script>
 import { HTTP } from '../components/http'
+import ImageLesson from '../components/ImageLesson'
 export default {
   name: 'PhotoLesson',
+  components: { ImageLesson },
   data () {
     return {
       model: { },
