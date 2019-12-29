d14154a9b09ac713393aac2f319e5e1d860877e6
Irina Bratkova <bratckova.i@yandex.ru>
2019-12-29 11:55:18 +0300

    Переход на главную добавлен в шапку App.vue

--- phphandmade/frontend/src/App.vue
+++ phphandmade/frontend/src/App.vue
@@ -4,10 +4,10 @@
     <section v-if="showDefaultLayout">
       <header>
         <nav class="navbar navbar-expand-lg navbar-light bg-light">
-          <a class="navbar-brand" href="#">
+          <router-link class="navbar-brand" to="/">
             <span style="color: skyblue">MADE</span>
             <span style="color: lightpink">HANDMADE</span>
-          </a>
+          </router-link>
           <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"/>
           </button>
