480b609e8534014dce63a9ddd0ac7cb115049722
Irina Bratkova <bratckova.i@yandex.ru>
2020-01-03 11:49:17 +0300

    поправлено отображение фотоурока (марджины)

--- phphandmade/frontend/src/views/PhotoLesson.vue
+++ phphandmade/frontend/src/views/PhotoLesson.vue
@@ -3,7 +3,7 @@
     <img class="img-fluid img-logo my-n3" src="img/ls4.png">
     <div class="container shadow rounded w-75 my-3 border border-yellow">
 
-      <div class="row row-content-center mt-3 mb-3 p-1 jumbotron-position shadow rounded-bookmark border border-olive">
+      <div class="row row-content-center m-3 p-1 jumbotron-position shadow rounded-bookmark border border-olive">
         <div align="center" style="margin-bottom: 1rem; margin-left: 1rem">
           <span class="text-radiance-lime" style="font-family:Lobster, cursive; font-size: xx-large;" > {{model.title}}</span>
           <div>
@@ -15,14 +15,14 @@
         </div>
       </div>
 
-      <div class="row row-content-center mb-3 p-1 jumbotron-position shadow rounded-bookmark border border-pink">
+      <div class="row row-content-center m-3 p-1 jumbotron-position shadow rounded-bookmark border border-pink">
         <div align="center" style="margin-bottom: 1rem; margin-left: 1rem">
           <span class="text-radiance-blue" style="font-family:Lobster, cursive; font-size:x-large;" >Краткое описание урока</span>
           <p>{{model.description}}</p>
         </div>
       </div>
 
-      <div class="row row-content-center mb-3 p-1 jumbotron-position shadow rounded-bookmark border border-green">
+      <div class="row row-content-center m-3 p-1 jumbotron-position shadow rounded-bookmark border border-green">
         <div align="center" style="margin-bottom: 1rem; margin-left: 1rem">
           <span class="text-radiance-yellow" style="font-family:Lobster, cursive; font-size:x-large;" >Материалы</span>
           <p>{{model.materials}}</p>
